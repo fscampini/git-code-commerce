e775c83af1efa41cd622fc3351aa3031968cb154
Felipe Scampini <fscampini@gmail.com>
2016-03-20 20:54:38 -0300

    Produtos com Tags

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -69,7 +69,9 @@ Route::group(['prefix'=>'admin', 'where' => ['id'=> '[0-9]+']], function()
 });
 
 Route::get('/', 'StoreController@index');
-Route::get('/{id}',['as'=> 'category.products', 'uses'=> 'StoreController@categoryProducts']);
+Route::get('category/{id}', ['as' => 'store.category', 'uses' => 'StoreController@category']);
+Route::get('product/{id}', ['as' => 'store.product', 'uses' => 'StoreController@product']);
+Route::get('tag/{id}', ['as' => 'store.tag', 'uses' => 'StoreController@tag']);
 
 
 // ***************************************
